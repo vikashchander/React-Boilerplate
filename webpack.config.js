dfed5356919a8feafd8cc629c7462a2e59e30b48
vikashchander <vikashchander78384@gmail.com>
2021-05-12 21:40:04 +0530

    reconfig webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 let path = require("path");
+const moduleObj = {
+  loaders: [
+    { test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader"] },
+  ],
+};
+
 const client = {
   entry: { client: "./src/client/index.js" },
   target: "web",
@@ -6,10 +12,14 @@ const client = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist/public"),
   },
+  module: moduleObj,
 };
+
 const server = {
   entry: { server: "./src/server/index.js" },
   target: "node",
   output: { filename: "[name].js", path: path.resolve(__dirname, "dist") },
+  module: moduleObj,
 };
+
 module.exports = [client, server];
