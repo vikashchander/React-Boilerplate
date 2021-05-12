8e3539a902eac5cf883a6eacfcb6d04f74c51a1c
vikashchander <vikashchander78384@gmail.com>
2021-05-12 21:46:15 +0530

    install rimraf , webpack and reconfig webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,21 @@
 let path = require("path");
+let nodeExternals = require("webpack-node-externals");
 const moduleObj = {
   loaders: [
     { test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader"] },
   ],
 };
-
 const client = {
   entry: { client: "./src/client/index.js" },
   target: "web",
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist/public"),
-  },
+  output: { filename: "[name].js", path: path.resolve(__dirname, "dist") },
   module: moduleObj,
 };
-
 const server = {
   entry: { server: "./src/server/index.js" },
   target: "node",
   output: { filename: "[name].js", path: path.resolve(__dirname, "dist") },
   module: moduleObj,
+  externals: [nodeExternals()],
 };
-
 module.exports = [client, server];
