428e1bae527dfbaa1f54f31228eb20ec959865f4
Vikash Chander <28572584+vikashchander@users.noreply.github.com>
2021-05-24 23:18:09 +0530

    add: install and setup nodemon

--- src/client/app.js
+++ src/client/app.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 // import Styles from 'style-loader!css-loader?modules!./styles.css';
 export default class App extends Component {
   render() {
-    return <div >Welcome to React Boilerplate App</div>;
+    return <div >Welcome to React Boilerplate App
+      <i> vikash chande sdfasfdr</i>
+    </div>;
   }
 }
